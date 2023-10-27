87023b7c4d57832ec9d24b75eca935039eaf3357
Prabha <prabha.yadav@likeminds.community>
2023-10-27 16:46:32 +0530

    data layer locally linked

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './FeedSX/App';
-import LMFeedClient from 'testpackageforlikeminds';
+import LMFeedClient from 'likeminds-sdk';
 
+// console.log('lmFeedClient', LMFeedClient.)
 export const lmFeedClient = LMFeedClient.Builder()
   .setApiKey('69edd43f-4a5e-4077-9c50-2b7aa740acce')
   .setPlatformCode('rn')
